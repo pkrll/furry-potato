d7cf07ae66177305f48c16222965eb7add9dc1d7
Ardalan Samimi <ardalan.samimi@gmail.com>
2019-04-01 20:23:06 +0200

    Extended the publisher script

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
-.PHONY: all node add commit push
+.PHONY: all node add commit push save
 
-INPUT=
+DRAFTS=drafts
+STATIC=static
 POSTS=index.json
 P_APP=publisher.js
 
 all: node add commit
 
 node:
-	node $(P_APP) $(INPUT)
+	node $(P_APP)
 
 add:
-	git add $(INPUT)
+	git add $(STATIC)
 	git add $(POSTS)
 
 commit:
-	git commit -S -m "Added $$INPUT"
+	git commit -S -m "Published new post"
 
 push:
 	git push
+
+save:
+	git add $(DRAFTS)
+	git commit -S -m "Saved draft"
--- publisher.js
+++ publisher.js
@@ -1,38 +1,99 @@
 'use strict'
 const fs = require('fs');
+const color = require('./.support/color.js')();
 
-function printUsage() {
-	console.log("Usage: node " + process.argv[1] + " [markdown file]");
-	process.exit(0);
-}
+const paths = {
+	DRAFTS: "./drafts/",
+	STATIC: "./static/"
+};
 
-function processFile(input) {
-	const data  = fs.readFileSync(input, 'utf8');
-	const lines = data.split("\n").filter( (el) => el != '');
+class Publisher {
 
-	return {title: lines[0].slice(2), summary: lines[2]};
-}
+	static start(indexFile, option) {
+		const publisher = new Publisher(indexFile);
+		publisher.run();
+	}
 
-function start(indexFile) {
-	const argc = process.argv.length;
-	if (argc < 3) printUsage();
+	constructor(indexFile) {
+		this.indexFile = indexFile;
+	}
 
-	const inputFile = process.argv[2];
-	const metadata  = processFile(inputFile);
+	processFile(input) {
+		const data  = fs.readFileSync(input, 'utf8');
+		const lines = data.split("\n").filter( (element) => element != '');
+    const meta  = {
+			title: (lines.length > 1) ? lines[0].slice(2) : 'N/A',
+			summary: (lines.length > 2) ? lines[2] : 'N/A'
+		};
 
-	const date = new Date();
-	metadata.date = date.toJSON();
-	metadata.path = inputFile.substr(inputFile.lastIndexOf('/') + 1);
-	fs.readFile(indexFile, (err, data) => {
-		if (err) throw err;
+		return meta;
+	}
 
-		let content = JSON.parse(data);
-		content.posts.unshift(metadata);
-
-		fs.writeFile(indexFile, JSON.stringify(content, null, 2), function(err) {
-			if(err) throw err;
+	run(option) {
+		const readline = require('readline').createInterface({
+			input: process.stdin,
+			output: process.stdout
 		});
-	});
+
+		fs.readdir(paths.DRAFTS, (err, data) => {
+			const files = data.filter(file => file.endsWith(".md"));
+
+			if (files.length == 0) {
+				color.print("No unpublished files found.", color.colors.red);
+				process.exit(-1);
+			}
+
+			for (let index in files) {
+				const fileNum  = parseInt(index) + 1;
+				const fileName = files[index];
+				color.print(fileNum + ". " + fileName, color.colors.green);
+			}
+
+			readline.question("\nSelect a file to publish (or 'a' to abort): ", (index) => {
+				readline.close();
+				if (index == 'a') process.exit(-1);
+				this.selectFile(files, parseInt(index))
+			});
+		});
+	}
+
+	selectFile(files, index) {
+		if (index > files.length) {
+			color.print("Invalid selection!", color.colors.red);
+			process.exit(-1);
+		}
+
+		const filePath = paths.DRAFTS + files[index - 1];
+		this.publish(filePath);
+	}
+
+	publish(filePath) {
+		const date = new Date();
+
+		const metadata = this.processFile(filePath);
+		metadata.date  = date.toJSON();
+		metadata.path  = paths.STATIC + filePath.substr(filePath.lastIndexOf('/') + 1);
+
+		fs.readFile(this.indexFile, (err, data) => {
+			if (err) throw err;
+
+			let content = JSON.parse(data);
+			content.posts.unshift(metadata);
+
+			fs.writeFile(this.indexFile, JSON.stringify(content, null, 2), (err) => {
+				if(err) throw err;
+
+				this.move(filePath, metadata.path);
+			});
+		});
+	}
+
+	move(oldPath, newPath) {
+		fs.rename(oldPath, newPath, (err) => { if (err) throw err; });
+	}
+
 }
 
-start("index.json");
+const option = (process.argv.length > 2) ? process.argv[2] : null;
+
+Publisher.start("index.json", option);
